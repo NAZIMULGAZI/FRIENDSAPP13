ead66634322d8c340e622a93b341cc357a1d1b6c
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-03 13:56:04 +0530

    NEW COMMIT

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,16 +1,41 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
+// import { useSelector } from 'react-redux'
+// import { useNavigate } from 'react-router-dom';
+
+// const ProtectedRoutes = ({children}) => {
+//     const {user} = useSelector(store=>store.auth);
+//     const navigate = useNavigate();
+//     useEffect(()=>{
+//         if(!user){
+//             navigate("/login");
+//         }
+//     },[])
+//   return <>{children}</>
+// }
+
+// export default ProtectedRoutes;
+
+
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 
-const ProtectedRoutes = ({children}) => {
-    const {user} = useSelector(store=>store.auth);
-    const navigate = useNavigate();
-    useEffect(()=>{
-        if(!user){
-            navigate("/login");
-        }
-    },[])
-  return <>{children}</>
+const ProtectedRoutes = ({ children }) => {
+  const { user } = useSelector(store => store.auth);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) return null; // or a loader/spinner
+
+  return <>{children}</>;
 }
 
 export default ProtectedRoutes;
